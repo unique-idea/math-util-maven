901c266596bf5683042b265310860ac1b4a4e1c7
unique-idea <lequanthanhbp2019@gmail.com>
2022-11-24 21:42:47 +0700

    upload project

---
+++ src/main/java/org/uniqueidea/mathutil/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uniqueidea.mathutil.main;
+/**
+ *
+ * @author lequa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi all, this app is built by using Maven");
+    }
+}
---
+++ src/test/java/org/uniqueidea/mathutil/core/MathUtilTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package org.uniqueidea.mathutil.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lequa
+ */
+public class MathUtilTest {
+    
+   @Test
+   public void testFactirialGivenRightArgumentReturnWell(){
+       assertEquals(100, 100);
+   }
+}
